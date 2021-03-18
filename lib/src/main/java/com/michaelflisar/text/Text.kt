2849be67e3fe9b970794ebfd01fbdead962fb553
mflisar <mflisar.development@gmail.com>
2021-03-18 13:32:03 +0100

    .

--- lib/src/main/java/com/michaelflisar/text/Text.kt
+++ lib/src/main/java/com/michaelflisar/text/Text.kt
@@ -1,7 +1,6 @@
 package com.michaelflisar.text
 
 import android.content.Context
-import android.opengl.Visibility
 import android.os.Parcelable
 import android.view.View
 import android.widget.TextView
